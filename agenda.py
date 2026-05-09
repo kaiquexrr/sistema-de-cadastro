860996313a4d319b037561c736a2e57b516f4c61
kaiquexrr <sousakaique178@gmail.com>
2026-05-08 21:40:00 -0300

    adicionando sistema de agenda com postgreSQL e variaveis de ambiente

--- agenda.py
+++ agenda.py
@@ -1,10 +1,14 @@
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 conn = psycopg2.connect(
-    dbname="agenda",
-    user="postgres",
-    password="103115",
-    host="localhost"
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST")
 )
 
 
